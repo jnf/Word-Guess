3b426d6d1c31b826cf042e671605ee301d9bbc51
jnf <jeremy.flores@gmail.com>
2016-02-25 15:00:35 -0800

    Add a word list to choose our word from.

--- word_guess.rb
+++ word_guess.rb
@@ -1,3 +1,12 @@
-class WordGuess #gigantic letters
+class WordGuess
+  def initialize
+    @words = {
+      "e" => %w(dog cat bug hat cap lit kin fan fin fun tan ten tin ton),
+      "m" => %w(plain claim brine crime alive bride skine drive slime stein jumpy),
+      "h" => %w(
+          machiavellian prestidigitation plenipotentiary quattuordecillion
+          magnanimous unencumbered bioluminescent circumlocution
+        )
+    }
 
 end
