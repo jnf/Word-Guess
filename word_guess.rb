eded931651fa37b1f73a207d9fb8642d9ee12b46
jnf <jeremy.flores@gmail.com>
2016-02-25 15:48:33 -0800

    Set the number of guesses a player gets based on their chosen difficulty mode.

--- word_guess.rb
+++ word_guess.rb
@@ -1,5 +1,6 @@
 class WordGuess
   def initialize
+    # possible words, selected at random
     @words = {
       "e" => %w(dog cat bug hat cap lit kin fan fin fun tan ten tin ton),
       "m" => %w(plain claim brine crime alive bride skine drive slime stein jumpy),
@@ -9,8 +10,21 @@ class WordGuess
         )
     }
 
-    @word = @words[set_mode].sample
-    puts "Your word is #{ @word }"
+    # players attempts allowed by difficulty
+    @tries = {
+      "e" => 10,
+      "m" => 6,
+      "h" => 4
+    }
+
+    # ask the user to set the game mode
+    mode = set_mode
+
+    @word    = @words[mode].sample # chosen word; players try to guess this
+    @guesses = @tries[mode] # how many tries the player gets
+
+    # debugging for now
+    puts "Your word is #{ @word }. You have #{ @guesses } guesses."
   end
 
   def set_mode
